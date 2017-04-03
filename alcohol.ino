0073afe344fe2fed9993cc0d85d14acbaa1bf907
shivamsehgal <shivamsehgal019@gmail.com>
2017-04-03 22:08:46 +0530

    Add files via upload

---
+++ alcohol.ino
@@ -0,0 +1,83 @@
+int LED_1 = 1;
+int LED_2 = 2;
+int LED_3 = 3;
+int LED_4 = 4;
+int LED_5 = 5;
+
+int alcohol = A0;
+
+int value = 0;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 1b as an output.
+  Serial.begin(9600);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+
+  pinMode(alcohol, INPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+
+  value = analogRead(alcohol);
+  Serial.println(value);
+
+  if (value < 200)
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, HIGH);
+    digitalWrite(LED_5, HIGH);
+  }
+
+  else if (value < 400)
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, LOW);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+    digitalWrite(LED_5, LOW);
+  }
+
+  else if (value < 500)
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, LOW);
+    digitalWrite(LED_4, LOW);
+    digitalWrite(LED_5, LOW);
+  }
+
+  else if (value < 600)
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, LOW);
+    digitalWrite(LED_5, LOW);
+  }
+
+  else if (value < 800)
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, HIGH);
+    digitalWrite(LED_5, LOW);
+  }
+
+  else
+  {
+    digitalWrite(LED_1, HIGH);
+    digitalWrite(LED_2, HIGH);
+    digitalWrite(LED_3, HIGH);
+    digitalWrite(LED_4, HIGH);
+    digitalWrite(LED_5, HIGH);
+  }
+}
